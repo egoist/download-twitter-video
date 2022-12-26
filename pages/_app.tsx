a96506476163097e6c3e340a13dbeba21afbb8b7
EGOIST <hi@egoist.dev>
2022-12-26 12:22:10 +0800

    adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 import "../css/tailwind.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV === "production" && <Analytics />}
+    </>
+  )
 }
 
 export default MyApp
